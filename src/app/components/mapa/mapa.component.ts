acea57d28e4276f8c790753eee3b68cf69359106
israel <israel.bejarano.a@gmail.com>
2019-04-07 22:34:35 +0200

    funcionalidad basica mapa

--- src/app/components/mapa/mapa.component.ts
+++ src/app/components/mapa/mapa.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Lugar } from '../../interfaces/lugar';
 
 @Component({
   selector: 'app-mapa',
@@ -7,9 +8,83 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapaComponent implements OnInit {
 
+  @ViewChild('map') mapaElement: ElementRef;
+  map: google.maps.Map;
+  marcadores: google.maps.Marker[] = [];
+  infoWindows: google.maps.InfoWindow[] = [];
+
+  lugares: Lugar[] = [
+    {
+      nombre: 'Udemy',
+      lat: 37.784679,
+      lng: -122.395936
+    },
+    {
+      nombre: 'Bahía de San Francisco',
+      lat: 37.798933,
+      lng: -122.377732
+    },
+    {
+      nombre: 'The Palace Hotel',
+      lat: 37.788578,
+      lng: -122.401745
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
+    this.cargarMapa();
+  }
+
+  cargarMapa() {
+    const latLng = new google.maps.LatLng(37.784679, -122.395936);
+    const mapaOpciones: google.maps.MapOptions = {
+      center: latLng,
+      zoom: 13,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    this.map = new google.maps.Map(this.mapaElement.nativeElement, mapaOpciones);
+    for (const lugar of this.lugares) {
+      this.agregarMarcador(lugar);
+    }
+  }
+
+  agregarMarcador(marcador: Lugar) {
+    console.log(marcador);
+    const latLng = new google.maps.LatLng(marcador.lat, marcador.lng);
+    const marker = new google.maps.Marker({
+      map: this.map,
+      animation: google.maps.Animation.DROP,
+      position: latLng,
+      draggable: true
+    });
+    this.marcadores.push(marker);
+    const contenido = `<b>${marcador.nombre}</b>`;
+    const infoWindow = new google.maps.InfoWindow({
+      content: contenido
+    });
+
+    this.infoWindows.push(infoWindow);
+
+    google.maps.event.addDomListener(marker, 'click', () => {
+      this.infoWindows.forEach(infoW => infoW.close());
+      infoWindow.open(this.map, marker);
+    });
+    google.maps.event.addDomListener(marker, 'dblclick', (coors: any) => {
+      console.log(coors);
+      marker.setMap(null);
+      // TODO disparar evento socket para borrar el marker
+    });
+    google.maps.event.addDomListener(marker, 'drag', (coors: any) => {
+      const nuevoMarcador = {
+        lat: coors.latLng.lat(),
+        lng: coors.latLng.lng(),
+        nombre: marcador.nombre
+      };
+      console.log(nuevoMarcador);
+      // TODO disparar evento socket para mover el marker
+    });
   }
 
 }
